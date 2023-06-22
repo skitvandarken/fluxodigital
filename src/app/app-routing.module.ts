dea7ebbe240c056da8d7a6aa3c6184d8f71aaf22
Pedro Jorge <skitvandarken2013@gmail.com>
2023-06-22 17:07:20 +0100

    Página inicial conluida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './layout/clientes/clientes.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { FormularioComponent } from './layout/formulario/formulario.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SliderComponent } from './layout/slider/slider.component';
+import { AcercaComponent } from './paginas/acerca/acerca.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { PortfolioComponent } from './paginas/portfolio/portfolio.component';
+import { ServicosComponent } from './paginas/servicos/servicos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //raiz
+  { path: '', component: InicioComponent },
+  //layout
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'slider', component: SliderComponent },
+  //páginas
+  { path: 'acerca', component: AcercaComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'servicos', component: ServicosComponent },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SliderComponent } from './layout/slider/slider.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { ServicosComponent } from './paginas/servicos/servicos.component';
+import { AcercaComponent } from './paginas/acerca/acerca.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { FormularioComponent } from './layout/formulario/formulario.component';
+import { ClientesComponent } from './layout/clientes/clientes.component';
+import { PortfolioComponent } from './paginas/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent,
+    InicioComponent,
+    ServicosComponent,
+    AcercaComponent,
+    ContactoComponent,
+    FooterComponent,
+    FormularioComponent,
+    ClientesComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
